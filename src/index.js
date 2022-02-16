f3ad79e3329e2e72822089cd8d87fb3c9986a9f4
StevenVallarsa <orionsjv@gmail.com>
2022-02-15 19:42:16 -0500

    fun stuff

--- src/index.js
+++ src/index.js
@@ -1,65 +1,36 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Header = props => {
-  return (
-    <h1>
-      It's {props.day}, I'm in love! ... with {props.name}
-    </h1>
-  );
-};
+const names = ["Julie", "Jessica", "Jill", "Juliana"];
 
-const Login = props => {
-  return (
-    <div>
-      <p>Please login!</p>
-      <button onClick={() => props.onClick()}>Sign In</button>
-    </div>
-  );
-};
+const App = props => {
+  const [type, setType] = useState("");
 
-const Signout = props => {
-  return <button onClick={() => props.onClick()}>Sign Out</button>;
-};
-
-const Layout = props => {
-  return (
-    <div
-      style={{
-        width: "50%",
-        color: "white",
-        backgroundColor: "skyblue",
-        padding: "10px",
-        margin: "10px",
-      }}
-    >
-      {props.children}
-    </div>
-  );
-};
-
-const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-
-  const onClick = () => {
-    setIsAuthenticated(prev => !prev);
+  const handleChange = e => {
+    setType(e.target.value);
   };
   return (
-    <div>
-      <Layout>
-        <Header day="Friday" name="Kim" />
-      </Layout>
-      <Layout>
-        <h1>I want you!</h1>
-        {isAuthenticated && (
-          <>
-            <h1>Welcome Back!</h1>
-            <Signout onClick={onClick} />
-          </>
-        )}
-        {!isAuthenticated && <Login onClick={onClick} />}
-      </Layout>
-    </div>
+    <>
+      <h1>The J Squad</h1>
+      <ul>
+        {names.map((name, index) => (
+          <Person index={index} name={name} />
+        ))}
+      </ul>
+      <input onChange={handleChange} type="text" placeholder="Type Me" />
+      <h1>{type}</h1>
+    </>
+  );
+};
+
+const Person = props => {
+  const handleClick = e => {
+    alert(props.name);
+  };
+  return (
+    <li key={props.index} onClick={e => handleClick(e)}>
+      {props.name}
+    </li>
   );
 };
 
