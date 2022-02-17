47276e628f898ed82deaa35d30c8e69fcbc6e9bb
StevenVallarsa <orionsjv@gmail.com>
2022-02-17 14:36:30 -0500

    setting baseline

--- src/index.js
+++ src/index.js
@@ -2,33 +2,11 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = props => {
-  const [mouse, setMouse] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    document.addEventListener("mousemove", mouseMoveEvent);
-    return () => {
-      document.removeEventListener("mousemove", mouseMoveEvent);
-    };
-  }, []);
-
-  const mouseMoveEvent = e => {
-    setMouse({ x: e.pageX, y: e.pageY });
-  };
   return (
     <>
-      <h1>
-        X: {mouse.x} Y: {mouse.y}
-      </h1>
+      <h1>USER</h1>
     </>
   );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-const App2 = props => {
-  return <h1 style={{ color: "red" }}>SECOND APP</h1>;
-};
-
-setTimeout(() => {
-  ReactDOM.render(<App2 />, document.getElementById("root"));
-}, 3000);
