55e8537ac2447967d92706ba06248c5294c588c1
StevenVallarsa <orionsjv@gmail.com>
2022-02-16 19:51:17 -0500

    still more playing around with useState

--- src/index.js
+++ src/index.js
@@ -2,35 +2,78 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const names = ["Julie", "Jessica", "Jill", "Juliana"];
+const nums = [...Array(50).keys()];
 
-const App = props => {
-  const [type, setType] = useState("");
-
-  const handleChange = e => {
-    setType(e.target.value);
-  };
-  return (
-    <>
-      <h1>The J Squad</h1>
-      <ul>
-        {names.map((name, index) => (
-          <Person index={index} name={name} />
-        ))}
-      </ul>
-      <input onChange={handleChange} type="text" placeholder="Type Me" />
-      <h1>{type}</h1>
-    </>
-  );
+const LongNumbers = props => {
+  return nums.map(num => <h1>{num}</h1>);
 };
 
-const Person = props => {
-  const handleClick = e => {
-    alert(props.name);
+const AbsoluteBox = props => {
+  return <div className="red"></div>;
+};
+
+const App = props => {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+    isEmployed: false,
+    isSkilled: true,
+  });
+
+  const handleNameChange = e => {
+    console.log(e.target.type, e.target.name);
+    if (e.target.type === "checkbox") {
+      if (e.target.name === "isEmployed") {
+        setName(prev => ({ ...prev, isEmployed: !prev.isEmployed }));
+      } else {
+        setName(prev => ({ ...prev, isSkilled: !prev.isSkilled }));
+      }
+    } else {
+      setName({ ...name, [e.target.name]: e.target.value });
+    }
+    console.log(name);
   };
+
+  const iveBeenClicked = e => {
+    console.log(e.target.innerHTML);
+    alert(e.target.innerHTML);
+  };
+
   return (
-    <li key={props.index} onClick={e => handleClick(e)}>
-      {props.name}
-    </li>
+    <>
+      <h1>Say My Name!</h1>
+      <input
+        name="firstName"
+        onChange={e => handleNameChange(e)}
+        type="text"
+        placeholder="First Name"
+        value={name.firstName}
+      />
+      <input name="lastName" onChange={e => handleNameChange(e)} type="text" placeholder="Last Name" />
+      <h1>
+        My Name Is {name.firstName} {name.lastName}
+      </h1>
+      <div className="container">
+        <div className="item">
+          <input type="checkbox" name="isEmployed" defaultChecked={name.isEmployed} onChange={handleNameChange} />
+          isEmployed
+        </div>
+        <div className="item">
+          <input type="checkbox" name="isSkilled" defaultChecked={name.isSkilled} onChange={handleNameChange} />
+          isSkilled
+        </div>
+      </div>
+
+      <ul>
+        {names.map((name, index) => (
+          <li key={index} onClick={e => iveBeenClicked(e)}>
+            {name}
+          </li>
+        ))}
+      </ul>
+      <LongNumbers />
+      <AbsoluteBox />
+    </>
   );
 };
 
