e8bc5bf979ba7b756c70eafa5cc1bdf8a2c003c5
StevenVallarsa <orionsjv@gmail.com>
2022-02-17 11:35:43 -0500

    learned how to remove event listeners

--- src/index.js
+++ src/index.js
@@ -3,19 +3,32 @@ import ReactDOM from "react-dom";
 
 const App = props => {
   const [mouse, setMouse] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
-    document.addEventListener("mousemove", e => {
-      setMouse({ x: e.pageX, y: e.pageY });
-    });
+    document.addEventListener("mousemove", mouseMoveEvent);
+    return () => {
+      document.removeEventListener("mousemove", mouseMoveEvent);
+    };
   }, []);
 
+  const mouseMoveEvent = e => {
+    setMouse({ x: e.pageX, y: e.pageY });
+  };
   return (
     <>
       <h1>
-        X: {mouse.x} - Y: {mouse.y}
+        X: {mouse.x} Y: {mouse.y}
       </h1>
     </>
   );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const App2 = props => {
+  return <h1 style={{ color: "red" }}>SECOND APP</h1>;
+};
+
+setTimeout(() => {
+  ReactDOM.render(<App2 />, document.getElementById("root"));
+}, 3000);
